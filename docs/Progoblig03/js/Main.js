650c69f29369429772b7740a0f5c81a001666925
Bulldog2910 <mat.nik.jorgensen@gmail.com>
2023-11-15 11:37:34 +0100

    Fin start på Nettsiden
    Mangler innhold til alle untatt Mathias

---
+++ docs/Progoblig03/js/Main.js
@@ -0,0 +1,28 @@
+let medlemmer = ['Mathias', 'Vetle', 'Joakim', 'Cornelia', 'Theodor', 'Anette'];
+let persbilde = [ "./images/Mathias.png", './images/Vetle.png',  "./images/Joakim.png",  "./images/Cornelia.png", "./images/Theodor.png" ,"./images/Anette.png",]
+let telefonNummer = ['+ 47 98316662', '1', '2', '3', '4', '5']
+let mail = ['mat.nik.jorgensen@gmail.com', '1', '2', '3', '4', '5']
+let quotes = ['“If a person can’t be calm for long, they can’t be happy for long, because happiness is calmness.”', '1', '2', '3', '4', '5' ]
+
+for( let i = 0; i < medlemmer.length; i++){
+const tittel = document.getElementById("perstittel" + i );
+tittel.innerHTML = medlemmer[i];
+tittel.style.paddingLeft = "50px";
+tittel.style.fontSize = "50px";
+tittel.style.color = "white";
+const teleNum = document.getElementById("tlf" + i );
+teleNum.innerHTML = telefonNummer[i]
+const post = document.getElementById("mail" + i );
+post.innerHTML = mail[i]
+const src = document.getElementById("Bilde" + i)
+let Bilde = document.createElement("img")
+Bilde.src = persbilde[i];
+src.appendChild(Bilde)
+
+const quotestext = document.getElementById("Tekst" + i)
+quotestext.innerHTML = quotes[i]
+}
+
+//document.getElementById("flytt" + i).onclick = function () {
+ //   location.href ="./Medlemmerinfo/Mathias.html";
+//}
