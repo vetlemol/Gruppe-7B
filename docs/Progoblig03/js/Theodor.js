a762b149fe9420279677eb57f048728b3be1e17f
theodorkp <theodorkp1@gmail.com>
2023-11-22 13:59:11 +0100

    Endret posisjon på figurer

--- docs/Progoblig03/js/Theodor.js
+++ docs/Progoblig03/js/Theodor.js
@@ -2,27 +2,27 @@ const canvas = document.getElementById("myCanvas1");
 const ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.arc(105, 75, 50, 0, 2 * Math.PI);
+ctx.arc(150, 75, 50, 0, 2 * Math.PI);
 ctx.lineWidth = 5;
 ctx.strokeStyle = "blue";
 ctx.stroke();
 
 ctx.beginPath();
-ctx.arc(205, 75, 50, 0, 2 * Math.PI);
+ctx.arc(250, 75, 50, 0, 2 * Math.PI);
 ctx.strokeStyle = "black";
 ctx.stroke();
 
 ctx.beginPath();
-ctx.arc(305, 75, 50, 0, 2 * Math.PI);
+ctx.arc(350, 75, 50, 0, 2 * Math.PI);
 ctx.strokeStyle = "red";
 ctx.stroke();
 
 ctx.beginPath();
-ctx.arc(155, 165, 50, 0, 2 * Math.PI);
+ctx.arc(200, 165, 50, 0, 2 * Math.PI);
 ctx.strokeStyle = "yellow";
 ctx.stroke();
 
 ctx.beginPath();
-ctx.arc(255, 165, 50, 0, 2 * Math.PI);
+ctx.arc(300, 165, 50, 0, 2 * Math.PI);
 ctx.strokeStyle = "green";
 ctx.stroke();
