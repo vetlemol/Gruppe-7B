a5fb1d54ead96413490eefe55d7b7d11af5be4e7
vetlemol <144101937+vetlemol@users.noreply.github.com>
2023-11-22 15:01:01 +0100

    Update Anette.js

--- docs/Progoblig03/js/Anette.js
+++ docs/Progoblig03/js/Anette.js
@@ -1,39 +1,20 @@
 var canvas1 = document.getElementById("myCanvas4");
 var context = canvas1.getContext("2d");
 
-
-
-
-// the triangle
+//ny trekant
 context.beginPath();
-context.moveTo(108, 0.0);
-context.lineTo(141, 70);
-context.lineTo(218, 78.3);
-context.lineTo(162, 131);
-context.lineTo(175, 205);
-context.lineTo(108, 170);
-context.lineTo(41.2, 205);
-context.lineTo(55, 131);
-context.lineTo(108, 0.0);
-context.closePath();
-context.fillStyle = "red";
+context.moveTo(350, 135);
+context.lineTo(300, 150);
+context.lineTo(350, 100);
+context.lineTo(400, 150);
+context.lineTo(350, 135);
+context.fillStyle = 'red';
 context.fill();
-
-/*
-ctx.fillStyle = "blue";
-ctx.beginPath();
-ctx.moveTo(108, 0.0);
-ctx.lineTo(141, 70);
-ctx.lineTo(218, 78.3);
-ctx.lineTo(162, 131);
-ctx.lineTo(175, 205);
-ctx.lineTo(108, 170);
-ctx.lineTo(41.2, 205);
-ctx.lineTo(55, 131);
-ctx.lineTo(1, 78);
-ctx.lineTo(75, 68);
-ctx.lineTo(108, 0);
-ctx.closePath();
-context.fillStyle = "";
-context.strokeRect(130, 300, 200, 50);
-*/
+//stjerne
+context.beginPath();
+context.moveTo(350, 135);
+context.lineTo(400, 115);
+context.lineTo(300, 115);
+context.lineTo(350, 135);
+context.fillStyle = 'red';
+context.fill();
